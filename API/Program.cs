1430ebacb69216463d358e1e40dfb167bbb3082b
rehanshami <77270784+rehanshami@users.noreply.github.com>
2024-11-30 12:25:43 +1300

    feat: Add create, edit, details and delete handlers

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,14 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy => {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
 
-
+builder.Services.AddAplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
